fbf1fa261b33b8f2cc7f85894ce1ee2e1565cddf
Alyssa Ross <hi@alyssa.is>
2015-04-15 15:17:21 +0100

    Reply to mentions

--- bots.rb
+++ bots.rb
@@ -1,6 +1,13 @@
 require "open-uri"
 require "nokogiri"
 require "twitter_ebooks"
+require "rest-client"
+
+Bulbapedia = RestClient::Resource.new("http://bulbapedia.bulbagarden.net")
+
+def Bulbapedia.go(term)
+  self["w/index.php"].get params: { search: term }
+end
 
 def trivia(page)
   trivia_header = page.css("#Trivia").first
@@ -12,6 +19,10 @@ def already_used?(trivia)
   false
 end
 
+def search_results?(url)
+
+end
+
 def tweet_content(title, url, content)
   "#{title} #{url}\n#{content}"
 end
@@ -20,21 +31,22 @@ def tweet_length(title, content)
   tweet_content(title, "", content).length + 22 # length of url
 end
 
-def random_trivia_from_page(page, url)
+def random_trivia_from_response(response, format: "%{title} %{url}\n%{content}")
+  page = Nokogiri::HTML(response.to_str)
+  page.css("sup").remove
   title = page.css("#firstHeading").text
   options = trivia(page) || []
   options.reject! do |option|
     tweet_length(title, option) > 140 || already_used?(option)
   end
-  options.map { |content| tweet_content(title, url, content) }
+  format_args = { title: title, url: response.request.url }
+  options.map { |content| format % format_args.merge(content: content) }
 end
 
 def random_trivia
   until option ||= nil
-    response = open("http://bulbapedia.bulbagarden.net/wiki/Special:Random")
-    page = Nokogiri::HTML response.read
-    options = random_trivia_from_page(page, response.base_uri)
-    option = options.sample
+    response = Bulbapedia["wiki/Special:Random"].get
+    option = random_trivia_from_response(response).sample
   end
   option
 end
@@ -52,6 +64,25 @@ class Bulbatrivia < Ebooks::Bot
       tweet random_trivia
     end
     tweet random_trivia
+  end
+
+  def on_mention(mention)
+    text = meta(mention).mentionless
+    response = Bulbapedia.go(text)
+
+    if response.request.url.start_with? "http://bulbapedia.bulbagarden.net/w/index.php"
+      answer = meta(mention).reply_prefix + "Bulbapedia doesn't have an article about "
+      answer += answer.length + text.length > 140 ? "that" : text
+      reply mention, answer
+      return
+    end
+
+    options = random_trivia_from_response(response, format: "%{content}")
+    answer = meta(mention).reply_prefix
+    answer += options.sample || ""
+    answer += " #{response.request.url}" if answer.length <= 127
+    answer.gsub! "  ", " "
+    reply mention, answer
   end
 end
 
