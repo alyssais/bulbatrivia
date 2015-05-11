e7f7361244214236e78385f65877fed114e705a8
Alyssa Ross <hi@alyssa.is>
2015-05-11 11:56:17 +0100

    Use environment variable for username

--- bots.rb
+++ bots.rb
@@ -1,6 +1,6 @@
 require_relative "lib/bulbatrivia"
 
-Bulbatrivia::Bot.new("bulbatrivia") do |bot|
+Bulbatrivia::Bot.new(ENV["TWITTER_USERNAME"]) do |bot|
   bot.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
   bot.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
   bot.access_token = ENV["TWITTER_ACCESS_TOKEN"]
